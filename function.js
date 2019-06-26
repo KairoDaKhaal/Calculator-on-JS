0cd64aab5e27f453bdddb0cced742f1836507f87
Gleb Khineev <KhineevGleb@icloud.com>
2019-06-27 01:07:27 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,43 @@
+function first(){
+	kalculator=document.getElementById("pervoi").value;
+	kalculator2=document.getElementById("vtoroi").value;
+	alert(kalculator*1 + kalculator2*1);
+}
+function second(){
+	kalculator=document.getElementById("pervoi").value;
+	kalculator2=document.getElementById("vtoroi").value;
+	alert(kalculator*1 - kalculator2*1);
+}
+function third(){
+	kalculator=document.getElementById("pervoi").value;
+	kalculator2=document.getElementById("vtoroi").value;
+	alert(kalculator*1 * kalculator2*1)}
+function fourd(){
+	kalculator=document.getElementById("pervoi").value;
+	kalculator2=document.getElementById("vtoroi").value;
+	alert(kalculator*1 / kalculator2*1)}
+function fiveth(){
+	kalculator=document.getElementById("pervoi").value;
+	kalculator2=document.getElementById("vtoroi").value;
+	alert(kalculator*1 % kalculator2*1)}	
+
+
+function sixth(){
+	var a=1*document.getElementById("pervoi").value;
+	var b=1*document.getElementById("vtoroi").value;
+	if (a>b) {alert(a + "Наибольшее число")}	
+	else{alert(b + "Наибольшее число")}}
+
+function seventh(){
+	var a=1*document.getElementById("pervoi").value;
+if (a<0) {alert(a+ "Отрицательное число")
+}else if(a>0){alert(a+ "Положительное число")}}	
+
+function eighth(){
+var b=1*document.getElementById("vtoroi").value;
+if (b/2) {alert(b+"Чётное число")
+else
+
+
+}
+
